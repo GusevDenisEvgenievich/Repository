116281271e6ecb9b9574af25793a00ac4af0ad12
GusevDenisEvgenievich <147509265+GusevDenisEvgenievich@users.noreply.github.com>
2023-12-04 17:38:46 +0500

    Add files via upload

---
+++ WpfApp13/AppicationData/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp13.AppicationData
+{
+    internal class AppConnect
+    {
+        public static AcademiaEntities model0db;
+    }
+}
