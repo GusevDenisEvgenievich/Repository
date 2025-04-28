80845b0f14fd847140b6ced81c3866b677c2fb65
GusevDenisEvgenievich <147509265+GusevDenisEvgenievich@users.noreply.github.com>
2025-04-28 15:29:01 +0500

    Add files via upload

---
+++ Гусев/kv_ex7_2/kv_ex7_2/Found.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kv_ex7_2
+{
+    /// <summary>
+    /// Логика взаимодействия для Found.xaml
+    /// </summary>
+    public partial class Found : Window
+    {
+        private string connection = "Host=localhost";
+        public Found()
+        {
+            InitializeComponent();
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
